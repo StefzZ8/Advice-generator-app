6480fc64fa9865fbd88e407353a05aa5b780cf57
Stefko1998 <Simjanovski998@gmail.com>
2024-05-13 16:40:33 +0200

    fetching api for random quote

---
+++ app.js
@@ -0,0 +1,21 @@
+function fetchedQuote() {
+
+  document.getElementById('roll-dice').addEventListener('click', function () {
+    fetch('https://api.adviceslip.com/advice')
+      .then(res => res.json())
+      .then(data => {
+        const fetchQuote = `"${data.slip.advice}"`
+        const quoteID = `#${data.slip.id}`
+
+        const getQuotes = document.querySelector('.quotes');
+        getQuotes.innerHTML = fetchQuote;
+
+        const getID = document.querySelector('.id-tag');
+        getID.innerHTML = quoteID;
+      })
+      .catch(err => {
+        console.error('Error fetching quote:', err);
+      })
+  })
+}
+fetchedQuote();
